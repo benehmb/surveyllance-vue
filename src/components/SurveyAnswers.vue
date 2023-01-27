cc171c1cbb18493a31857b1b4ff1b440fc8382b3
Patrick <patrick.schwarzmann@hs-weingarten.de>
2023-01-27 14:43:16 +0100

    dynamic Recent Survey

---
+++ src/components/SurveyAnswers.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-row>
+        <v-col>
+            {{answerText}}
+        </v-col>
+        <v-col>
+            <v-progress-linear value="30"></v-progress-linear>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+    name: "SurveyAnswers",
+    props: {
+        answerText: String,
+    },
+    data() {
+        return {
+            answer: this.answerText,
+        };
+    },
+};
+</script>
+
+<style>
+</style>
